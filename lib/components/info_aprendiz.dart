a16c35b886f693c57533588e5d50160e5dd75bf5
stevenpajarodev <stevenpajarodev@gmail.com>
2024-06-24 06:39:44 +0000

    codigo-barra-automatico

--- lib/components/info_aprendiz.dart
+++ lib/components/info_aprendiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:barcode_widget/barcode_widget.dart";
 
 class InfoAprendiz extends StatelessWidget {
   final String primerNombre;
@@ -65,18 +66,12 @@ class InfoAprendiz extends StatelessWidget {
                         fontWeight: FontWeight.normal,
                       ),
                     ),
-                    const SizedBox(
+                     SizedBox(
                       height: 60,
                       width: double.infinity,
                       child: Padding(
-                        padding: EdgeInsets.only(top: 15.0, right: 55.0),
-                        child: Image(
-                          image: NetworkImage(
-                            "https://d100mj7v0l85u5.cloudfront.net/s3fs-public/2022-10/futuro-codigo-de-barras.png",
-                          ),
-                          fit: BoxFit.cover,
-                          alignment: Alignment.centerLeft,
-                        ),
+                        padding:const EdgeInsets.only(top: 15.0, right: 60.0),
+                        child: BarcodeWidget(data: cc, barcode: Barcode.code39(),drawText: false,color: Colors.black,),
                       ),
                     ),
                   ],
